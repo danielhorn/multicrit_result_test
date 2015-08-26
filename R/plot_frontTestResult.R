9e91559f3f25d844222e0bca1bd853debd1e14d5
Daniel Horn <daniel.horn@tu-dortmund.de>
2015-08-26 14:14:54 +0200

    cleanup

--- R/plot_frontTestResult.R
+++ R/plot_frontTestResult.R
@@ -328,7 +328,7 @@ finalPlot = function(data, best.algo.order, split.vals, var.cols, algo.col, repl
   p = p + ggplot2::geom_line(size = 2)
   p = p + ggplot2::xlab(var.cols[1L]) + ggplot2::ylab(var.cols[2L])
   p = p + ggplot2::ggtitle("Reduced common Pareto front")
-  p = p + ggplot2::scale_colour_manual(values = colors)
+  p = p + ggplot2::scale_colour_manual(values = colors, name = algo.col)
   return(p)
 }
 

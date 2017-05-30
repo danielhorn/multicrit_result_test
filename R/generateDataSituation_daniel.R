4879150fdfd835353c3ea8e9f38b60ce94bdc528
rlpink <rosa.pink@tu-dortmund.de>
2017-05-30 13:26:22 +0200

    singleDataSituationData

--- R/generateDataSituation_daniel.R
+++ R/generateDataSituation_daniel.R
@@ -58,51 +58,6 @@ generateDataSituation = function(N, M, D, type, p, sigma, ...) {
   
   ##################################
   
-  singleDataSituationData = function(situation, split.points, algo.order, p, sigma) {
-    
-    # Remove one random Algo from Front
-    if (situation == 4L) {
-      # Only with prob p switch
-      if (rbinom(1, 1, p)) {
-        # Which algorithm to remove?
-        remove.id = sample(N, 1)
-        # Border cases 1 and N:
-        if (remove.id == 1L) {
-          new.split = split.points[-1L]
-        } else if (remove.id == N) {
-          new.split = split.points[-(N - 1L)]
-        } else{
-          # Sample the new split.point
-          left.split = split.points[remove.id - 1]
-          right.split = split.points[remove.id]
-          new.split = split.points
-          new.split[remove.id ] = runif(1, left.split, right.split)
-          new.split = new.split[-(remove.id - 1)]
-        }
-        res.list = list(
-          algo.order = c(algo.order[-remove.id], remove.id),
-          split.points = new.split
-        )
-        # ELSE: Wir machen keine Vertauschung, sondern geben die
-        # Defaults zurueck
-      } else {
-        res.list = list(
-          split.points = split.points,
-          algo.order = algo.order
-        )
-      }
-    }
-    
-    
-    if (situation == 9L) {
-      res.list = list(
-        split.points = sort(runif(N - 1)),
-        algo.order = c(sample(N), (N + 1) : (N + M))
-      )
-    }
-    return(res.list)
-  }
-  
   data.situation.params = lapply(1:D, function(i)
     singleDataSituationData(situation = type))
   
@@ -115,8 +70,7 @@ generateDataSituation = function(N, M, D, type, p, sigma, ...) {
       split.points = ds$split.points, algo.order = ds$algo.order,
       discretize.type = "NSGA-II_g", replications.type = "parameter-noise",
       k = 20L, replication = 10L)
-  }
-  )
+  })
   
   
 }

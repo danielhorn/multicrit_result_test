6b81de7cda3c858b6388d1a31a401d2ad4153c42
rlpink <rosa.pink@tu-dortmund.de>
2017-06-07 13:14:12 +0200

    documentation for singleDat(..) added

--- R/singleDataSituationData.R
+++ R/singleDataSituationData.R
@@ -1,4 +1,35 @@
-# FIXME: Rosa Dokumentiert noch
+#' Creates split points and algorithm order parameters to generate single data situations
+#'
+#' @param N [\code{integer}] \cr
+#'   Number of algorithms on the common pareto front.
+#' @param M [\code{integer}] \cr
+#'  Number of additional algorithms that are not on the common pareto front.
+#' @param situation [\code{integer}] \cr
+#'  Number of data situation to generate:
+#'  1: identical order of algorithms, split points identical
+#'  2: identical order of algorithms, split points noisy (same mean)
+#'  3: identical order of algorithms, split points differ ('complicated')
+#'  4: changed order, (mainly) identical split points; for each data set one algorithm that is
+#'     normally found on the common pareto front is missing (not on the front).
+#'  5: changed order: a normally dominated algorithm appears randomly on the common pareto front (reverse to 4)
+#'  6: changed order: dominated and non-dominated algorithm swapped (combines 4 and 5)
+#'  7: changed order: on p% of data sets two (non-dominated) algorithms are swapped, split points identical
+#'  8: random order, split points identical
+#'  9: random order and split points random
+#'  @param split.points [\code{numeric}]
+#'  Vector containing standard (true) split points
+#'  @param algo.order
+#'  Vector that contains the standard order of algorithms; integers from 1:(N+M) without duplicates.
+#'  @param p [\code{numeric}] \cr
+#'  Parameter for data.situation 4-7: probability for a missing/ inserted/ swapped algorithm on each data set. Between 0 and 0.4 (?)
+#'  @param sigma [\code{numeric}] \cr
+#'  Parameter for data.situation 2 - noise strength
+#'  
+#' 
+#' @return [\code{list}]
+#'  List that contains true split points, the names of the algorithms and validation data; and a list of lists (one for each data set), each containing the true (original) pareto landscape 
+#'  (functions that form the pareto fronts, true splitpoints, ...) - INCLUDING the names
+#'  of the algorithms.
 
 singleDataSituationData = function(situation, N, M, split.points, algo.order, p, sigma) {
   

e84cdfa38427e94e4968372ce34e4e25911c8c8d
rlpink <rosa.pink@tu-dortmund.de>
2017-08-02 14:55:15 +0200

    evaluatePortfolio erstellt, die anderen evaluate*-Funktionen fertiggestellt

---
+++ R/evaluatePortfolio.R
@@ -0,0 +1,27 @@
+#' Combined measures for the comparison of a true and an estimated common pareto front
+#' Returns global error in X dimension as well as optimality gap (based on hypervolume)
+#' for each data set.
+#'
+#' @param validation.obj
+#'   Result of \code{generateParetoLandscape}.
+#' @param portfolio  
+#'   Result of \code{MOSAP::selectPortfolio}. 
+#' 
+#' @return [\code{numeric}]
+#'  Result is a list of two elements:
+#'  - (global) X-Error
+#'  - optimality gap measure for each data set
+#'  
+#' @export
+evaluatePortfolio = function(validation.obj, portfolio){
+  x.error = evaluateWithXIntegral(validation.obj, portfolio)
+  all.ds = unique(validation.obj$valid.data$dataset)
+  opt.gaps = numeric(length = length(all.ds))
+  
+  for(ds in seq_along(all.ds)) {
+    opt.gaps[ds] = evaluateWithOptimalityGap(validation.obj, portfolio, all.ds[ds])
+  }
+  
+  result = list(x.error = x.error, opt.gaps = opt.gaps)
+  return(result)
+}
